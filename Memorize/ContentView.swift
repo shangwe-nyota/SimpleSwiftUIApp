edd9387567b6b10f6b50dbb718940bfe9dc39236
Shangwe Nyota <s.nyota@wustl.edu>
2024-08-26 23:59:33 -0500

    Integrated zstack and rectangle

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -33,21 +33,20 @@ struct ContentView: View {
         //Why does vstack not have open parenthesis?
         //A:Actually it could!
         //VStack is a view that takes in one argument which is a function that returns a bag of lego view that it can arrange
-        VStack(content: {
+        ZStack(content: {
             //Example of creating an Image struct that behaves like a view
             //The Image struct argument is "globe" with a named parameter systemName
-            Image(systemName: "globe")
-                .foregroundStyle(.green) //function in swift with parameters
             //Example of creating a Text struct
             //Text view parameters is the text hello cs... but the parameter doesnt have or need a name!
-            Text("Hello, CS 193P!").padding() //function call in swift
+            RoundedRectangle(cornerRadius: 12)
+            Text("👻").font(.largeTitle) //function call in swift
             //Both above structs act as a "View" ---> can stack them
         })
-        .font(.largeTitle) //everything in the vstack got the modifier
         .foregroundColor(.orange)//scoping of a view modifer matters
         //These functions aren't special in any sort of way.. we call it a "view modifier" they return a view, more specifically a modified view
         //Image is a struct that behaves like a view
         .imageScale(.large)
+        .padding()
     }
 }
 
